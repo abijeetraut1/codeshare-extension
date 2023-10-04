cc65c405092496d4b6add13bcb02290efca0f617
Abijeet <63050833+abijeetraut1@users.noreply.github.com>
2023-10-04 22:18:05 +0545

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const allRouter = require("./router/allRoutes");
 const bodyParser = require("body-parser");
 const port = 9980;
 
-mongoose.connect(`connection url`).then(() => {
+mongoose.connect(`mongodb+srv://guymail349:rWuqWVWW3Pvee4vm@codesend.c0tsj4d.mongodb.net`).then(() => {
     console.log("connected to database");
 }).catch(err => {
     console.log("failed to connect to database " + err);
@@ -14,7 +14,13 @@ mongoose.connect(`connection url`).then(() => {
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
+app.use("*", (req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
+    next();
+})
 
 app.use("/api/vscodeExtensions/v1/sendandstore", allRouter);
 
