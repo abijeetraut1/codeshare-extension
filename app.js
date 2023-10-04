6cddc87f25985fa29a20b96f0425f8b527f7d046
Kapil Tamang <70058377+KapilLopchanTamang@users.noreply.github.com>
2023-10-04 21:34:36 +0545

    added cors

--- app.js
+++ app.js
@@ -14,7 +14,13 @@ mongoose.connect(`connection url`).then(() => {
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
+app.use("*", (req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
+    next();
+})
 
 app.use("/api/vscodeExtensions/v1/sendandstore", allRouter);
 
